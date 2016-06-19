0905b5b8822a57eeadf022d7f8cb2133c0d208da
Brett Lyons <blyons@students.naropa.edu>
2016-06-18 19:49:00 -0600

    New branch, redid challenge1

--- web/elm/Main.elm
+++ web/elm/Main.elm
@@ -3,11 +3,7 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.App as Html
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
 import Mouse exposing (Position)
-import Svg exposing (Svg, circle)
-import Svg.Attributes exposing (viewBox, cx, cy, r)
-import Random exposing (..)
 import Task
 import Window exposing (Size)
 
@@ -16,72 +12,54 @@ import Window exposing (Size)
 
 
 type alias Model =
-    { clickCount : Int
-    , pos : Position
+    { pos : Position
     , winSize : Size
     }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { clickCount = 0
-      , pos = Position 0 0
+    ( { pos = Position 0 0
       , winSize = Size 0 0
       }
-    , getWinWidth
+    , getWindowWidth
     )
 
 
 
--- Helper fn to get the window size, useful as a Cmd .
+-- Helper fn to get the window size, useful as a Cmd
 
 
-getWinWidth : Cmd Msg
-getWinWidth =
-    Task.perform identity CurrentSize Window.size
+getWindowWidth : Cmd Msg
+getWindowWidth =
+    Window.size |> Task.perform identity CurrentSize
 
 
 
--- getRandomPos : Cmd Msg
--- getRandomPos =
---     Random.generate NewCircle (Random.pair (Random.int 1 380) (Random.int 1 380))
 -- UPDATE
 
 
 type Msg
-    = Increment
-    | Decrement
-    | Reset
+    = CurrentSize Size
     | CurrentPos Position
-    | CurrentSize Size
-
-
-
--- | NewCircle ( Int, Int )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Increment ->
-            ( { model | clickCount = model.clickCount + 1 }, Cmd.none )
-
-        Decrement ->
-            ( { model | clickCount = model.clickCount - 1 }, Cmd.none )
-
-        Reset ->
-            ( { model | clickCount = 0 }, Cmd.none )
-
-        CurrentPos position ->
-            ( { model | pos = position }, Cmd.none )
-
         CurrentSize size ->
             ( { model | winSize = size }, Cmd.none )
 
+        CurrentPos pos ->
+            ( { model | pos = pos }, Cmd.none )
 
 
--- NewCircle {x, y} ->
---     ( {model | })
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch [ Mouse.moves CurrentPos, Window.resizes CurrentSize ]
+
+
+
 -- VIEW
 
 
@@ -92,43 +70,17 @@ view model =
             model.winSize.width // 2
     in
         div []
-            [ button
-                [ class "btn btn-lg btn-primary", onClick Decrement ]
-                [ text " - " ]
-            , div [] [ text (toString model.clickCount) ]
-            , button
-                [ class "btn btn-lg btn-info", onClick Increment ]
-                [ text " + " ]
-            , p [] []
-            , button
-                [ class "btn btn-lg btn-danger", onClick Reset ]
-                [ text "Reset" ]
-            , p [] []
-            , code [] [ text (toString model.pos) ]
-            , code [] [ text (toString model.winSize.width) ]
+            [ code [] [ text (toString model.pos) ]
+            , code [] [ text (toString model.winSize) ]
             , code [] [ text (toString halfWidth) ]
             , p [] []
             , (if halfWidth > model.pos.x then
-                plate "Navy" "On The Left"
+                plate "Green" "Right"
                else
-                plate "Tomato" "On The Right"
+                plate "Navy" "Left"
               )
             , p [] []
-            , div [] [ text "Next thiny goes here?" ]
             ]
-
-
-
--- in this case, pos happens to be a conveniently defined type
--- from Mouse -- even though I won't actually use Mouse.position
--- for this.
-
-
-smallCircleMaker : Int -> Position -> Svg Msg
-smallCircleMaker radius pos =
-    Svg.svg
-        [ width 400, height 400, viewBox "0 0 400 400" ]
-        [ Svg.circle [ cx (toString pos.x), cy (toString pos.y), r "10" ] [] ]
 
 
 plate : String -> String -> Html Msg
@@ -148,11 +100,6 @@ plateStyle color =
     , ( "align-items", "center" )
     , ( "justify-content", "center" )
     ]
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.batch [ Mouse.moves CurrentPos, Window.resizes CurrentSize ]
 
 
 main : Program Never
